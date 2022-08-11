d3fa69f351a1c0f434edba34d96d8b2af9d21ea4
brunodlucka <bruno_lucka@hotmail.com>
2022-08-11 18:23:26 -0300

    controllers de agendamento e models agendamento

---
+++ projeto/src/main/java/com/projeto/prontmed/controller/AgendamentoController.java
@@ -0,0 +1,28 @@
+package com.projeto.prontmed.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AgendamentoController {
+
+	
+	@RequestMapping("/form")
+	public String form() {	
+		return "formagendamento";
+	}
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ projeto/src/main/java/com/projeto/prontmed/controller/HomepageController.java
@@ -0,0 +1,13 @@
+package com.projeto.prontmed.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomepageController {
+	@RequestMapping("/homepage")
+   	public String homepage() {	
+   		return "homepage";
+       }
+
+}
