3ac4a19c10f7fd58706300d5225c3132f56d32d6
BiancaBritto <britto.bianca02@gmail.com>
2022-08-15 19:13:53 -0300

    login

--- projeto/src/main/java/com/prontmed/projeto/resource/AdminController.java
+++ projeto/src/main/java/com/prontmed/projeto/resource/AdminController.java
@@ -19,12 +19,19 @@ import com.prontmed.projeto.servicos.AdminServico;
 public class AdminController {
 
         @Autowired
-        private  AdminServico adminServico;   
-      
+        private  AdminServico adminServico;
+
     @CrossOrigin(origins = "*")//perimite receber pos de requsisiçoes locais
     @PostMapping(path = "salvaAdmin",consumes = MediaType.APPLICATION_JSON_VALUE )
     public void salvaProntuario(@RequestBody final Admin admin){
         adminServico.salvaAdmin(admin);
+    }
+
+    @CrossOrigin(origins = "*")//perimite receber pos de requsisiçoes locais
+    @PostMapping(path = "login",consumes = MediaType.APPLICATION_JSON_VALUE )
+    public Admin loginUsuario(@RequestBody final Admin admin){
+        final Admin adminLogado = adminServico.login(admin);//realiza o login
+        return adminLogado;
     }
 
 }
--- projeto/src/main/java/com/prontmed/projeto/servicos/AdminServico.java
+++ projeto/src/main/java/com/prontmed/projeto/servicos/AdminServico.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Service;
 import com.prontmed.projeto.entidades.Admin;
 import com.prontmed.projeto.repositorios.AdminRepositorio;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class AdminServico {
 
@@ -15,6 +18,19 @@ public class AdminServico {
     public void salvaAdmin(final Admin admin) {
         adminRepositorio.save(admin);
     }
+
+    public Admin login(final Admin adminRequest) {
+        final List<Admin> todos = adminRepositorio.findAll();
+        //string filter está filtrando na lista o objeto com o mesmo email e senha
+        final List<Admin> usarioLogado =
+                todos.stream().filter(admin -> admin.getEmail().equals(adminRequest.getEmail()) && admin.getSenha().equals(adminRequest.getSenha())).collect(Collectors.toList());
+        if(usarioLogado.isEmpty()){
+            return null;
+        }else{
+            return usarioLogado.get(0);
+        }
+    }
+
 
 }
 
